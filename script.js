7504ed6a85a6385fa2d9a36611d09adfe1848196
Ana Julia <ajps.leal@unesp.br>
2026-08-10 20:19:43 -0300

    Falta arrumar a responsividade das sections( carrossel e categoria).

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ function showSlides() {
 }
 
 showSlides();
+
+
+const body = document.querySelector("body");
+const navHeader = document.querySelector(".navheader");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+// Abrir Menu Mobile
+menuBtn.onclick = () => {
+    navHeader.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+};
+
+// Fechar Menu Mobile
+cancelBtn.onclick = () => {
+    body.classList.remove("disabled");
+    navHeader.classList.remove("show");
+    menuBtn.classList.remove("hide");
+};
